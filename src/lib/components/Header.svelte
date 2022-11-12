6e14c1cd074d3dc7ff0bcf72d70ad3e056e9ec1c
merryt <tymerry@gmail.com>
2022-11-11 20:15:13 -0500

    nav cleanup

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -12,6 +12,22 @@
 <style>
 	nav {
 		width: var(--container-width);
-		margin: 0 auto;
+		margin: 24px auto 0 auto;
+		display: flex;
+		justify-content: flex-end;
+	}
+	nav > a {
+		text-decoration: none;
+		color: #fff;
+		margin-left: 2px;
+		padding: 10px 20px;
+		font-size: 18px;
+		font-family: var(--font-display);
+		border-radius: 8px;
+		transition: background-color 0.25s ease-in-out;
+	}
+
+	nav > a:hover {
+		background-color: rgba(0, 0, 0, 0.25);
 	}
 </style>

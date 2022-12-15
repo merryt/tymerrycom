54e8247232a110e289d43b8d8286febebc6f3378
merryt <tymerry@gmail.com>
2022-12-15 13:15:01 -0700

    updating about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,12 +1,13 @@
 <div>
+	<img src="/speaking2.jpg" alt="" class="about-hero-img" />
 	<h1>Who is Tyler Merry?</h1>
 	<p>
 		In the past I have been called a designer who does some dev stuff. Lately I have shifted to more
 		of a developer who sometimes does design work. I have spoken at a few small meetups, and helped
 		technical review books, but my favorite thing to do is to volunteer at the library teaching kids
 		to code, and at a local bike shop that helps people fix their bikes for free/cheap. I am a
-		lifelong learner and helper. For a picture of what I prioritize in live checkout<a
-			href="/blog/2021-recap-and-2022-goals">my goals from 2018</a
+		lifelong learner and helper. For a picture of what I prioritize in live checkout <a
+			href="/blog/2021-recap-and-2022-goals">my goals from 2021</a
 		>. I currently live in Colorado and
 		<a href="http://travelsaveandbemerry.com/">love to travel</a>. Some of my favorite places so far
 		have been Ljubljana and Kuala Lumpur. I like to build silly little programs that do stuff like
@@ -18,4 +19,26 @@
 		reach out to me perhaps send a message to
 		<a href="https://www.linkedin.com/in/merryt/">my linkedin</a>!
 	</p>
+	<h2>Not To Toot My Own Horn.. But Toot Toot</h2>
+	<ul>
+		<li>Regularly volunteer and teach with Denver Public Libraries code camps.</li>
+		<li>Technical reviewed two development</li>
+		<li>Books (Pro React, and Pro Vagrant)</li>
+		<li>Host and organizer of Denver UX.</li>
+		<li>Regular speaker at design and tech meetups</li>
+	</ul>
+	<h2>If You Would Like To Learn More About Me</h2>
+	<ul>
+		<li>www.dribbble.com/tymerry</li>
+		<li>https://www.behance.net/merry</li>
+	</ul>
 </div>
+
+<style>
+	.about-hero-img {
+		width: 50%;
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+	}
+</style>

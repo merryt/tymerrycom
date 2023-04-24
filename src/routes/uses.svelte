2b3f4d749628aea7141e605183df50357c50afc8
merryt <tymerry@gmail.com>
2023-04-24 15:49:37 +0200

    updating uses page

--- src/routes/uses.svelte
+++ src/routes/uses.svelte
@@ -1,6 +1,49 @@
-<ul>
-	<li>vscode</li>
-	<li>sveltekit</li>
-	<li>tom bign synapse</li>
-	<li>Asus ROG g14</li>
-</ul>
+<div>
+	<h1>This I own/use</h1>
+	I have a very small collection of things that I use very frequently. For the past few months I have
+	been traveling so my office/desk supplies are very much limited to what the plalce I am staying has
+	on has.
+
+	<h2>What do I use daily?</h2>
+	<strong>Physical Things</strong>
+	<ul>
+		<li>
+			<strong>Tom Bign Synapse</strong> - All my stuff when I travel location to location is stored in
+			carried around in a 25L backpack. I choose the Synapse mostly because of all the positive reviews
+			it has gotten online. I like it well enough, but probably not worth the price when so many other
+			great backpacks are out there
+		</li>
+		<li>
+			<strong>Asus ROG g14</strong> - My daily driver computer is a 14 inch Asus laptop. I originally
+			got it because I liked having a high powered video card+processor. I will probably end up running
+			linux on it shortly because my window management solution on windows is lacking
+		</li>
+		<li>
+			<strong>Nexstand</strong> - This is a knock off roost stand. Inhindsight I would have given the
+			money to the original creators and gotten the slightly smaller version to save a little bit of
+			weight
+		</li>
+		<li>
+			<strong>NuPhy Air v75</strong> Has been working great for me so far, probably could have saved
+			a little weight and size going with the v60, but the switches and keycaps are great. I have tried
+			a slew of mechanical keyboards in a variety of configurations. I previously traveled with a WASD
+			tkl, it was a bit larger but it worked.
+		</li>
+	</ul>
+	<strong>Software</strong>
+	<ul>
+		<li>
+			<strong>Anki</strong> - Open source flashcard program. I use it on both computer + phone. I throw
+			small facts that I need to recall quickly in there and drill myself daily
+		</li>
+		<li>
+			<strong>Memrise</strong> - I previously used duolingo + anki for vocab, but I have been enjoying
+			memrise a bit more lately. Both are probably a worse choice then talking with someone and making
+			mistakes.
+		</li>
+		<li>
+			<strong>VSCode</strong> - I spent some time getting good at emacs but it felt like my ide was broken
+			20% of the time and it was turning into a barrier to learning anything other then emacs
+		</li>
+	</ul>
+</div>
